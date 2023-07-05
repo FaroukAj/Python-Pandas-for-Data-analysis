8633fe5e6be1faeb4ff5b8613d3f89737ec008d4
Farouk Ajayi <79684324+FaroukAj@users.noreply.github.com>
2023-07-05 03:02:43 -0500

    Add files via upload
    In this data analysis project, I leveraged Python and the powerful Pandas library to perform comprehensive analysis on university data. Through a series of tasks, I demonstrated my proficiency in data manipulation, aggregation, and visualization techniques, extracting valuable insights to inform strategic decision-making within the academic institution.
    
    Key Tasks and Accomplishments:
    
    Student Enrollment Analysis: Utilizing Pandas, I retrieved and analyzed the total number of students in each field of study. This analysis provided valuable insights into enrollment patterns, enabling effective resource allocation and curriculum planning.
    Student Demographics: By calculating the average age of students in each specialization, I gained a deeper understanding of the academic pathways and demographic composition within different areas of study. This information proved instrumental in tailoring teaching approaches, designing support services, and fostering an inclusive learning environment.
    Graduation Projection: Through data analysis, I determined the number of students expected to graduate in each year. This forecasting capability facilitated graduation planning, resource management, and enabled proactive measures to ensure a smooth transition for graduating students.
    Financial Performance Assessment: Using Python and Pandas, I computed the total fees collected for each year of admission. This analysis shed light on revenue trends and financial performance, aiding in financial planning, investment decisions, and adjustments to tuition fee structures.
    Pricing Strategy and Financial Aid Evaluation: Leveraging Pandas, I calculated the average discount on fees for each field of study. This analysis helped evaluate the competitiveness and market demand for different programs, informing pricing strategies and financial aid policies to attract and retain students.
    Exceptional Case Identification: I identified students with the highest discount on fees, offering valuable insights into exceptional cases and the impact of financial aid programs. This analysis facilitated recognition of deserving students, assessment of aid program effectiveness, and identified areas for improvement.
    Financial Compliance Monitoring: Through data analysis, I retrieved the list of students who had not paid their fees for the current semester. This analysis enabled timely intervention, ensuring uninterrupted education and minimizing administrative issues caused by delayed payments.
    Resource Allocation: By identifying the field of study with the highest total fees collected, I contributed to resource allocation decisions, ensuring effective deployment of resources and investment in facilities, faculty, and student support services.
    Financial Compliance Assessment: Using Python and Pandas, I calculated the percentage of students who had paid their fees for the current semester. This analysis provided a measure of financial compliance and aided in budget planning and cash flow management.
    Cohort Identification: By identifying students with shared field of study, specialization, and expected year of graduation, I contributed to the creation of cohort-based programs, fostering collaboration, and targeted support initiatives. This analysis facilitated a sense of community, peer learning, and tailored support for these groups.
    Through this data analysis project, I showcased my expertise in Python and Pandas, effectively manipulating and analyzing large datasets to extract meaningful insights. My ability to derive actionable information from data contributes to evidence-based decision-making, ultimately driving organizational success within the academic domain.

---
+++ StudentData_pythonproject.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+import datetime
+import os
+
+
+# In[2]:
+
+
+os.getcwd()
+
+
+# In[3]:
+
+
+os.chdir('/Users/Flacko/Documents/Python')
+
+
+# In[5]:
+
+
+data = pd.read_csv('student_data-2 copy.csv')
+
+
+data.describe()
+
+
+# In[ ]:
+
+
+#Check for missing values 
+data.isnull().sum()
+
+
+# In[97]:
+
+
+data.head(50)
+
+
+# In[44]:
+
+
+#Calculate the average age of students in each specialization.
+
+
+#CONVERT 'Date_of_Birth' column from string to date datatype :
+
+data['Date_of_Birth'] = pd.to_datetime(data['Date_of_Birth'])
+
+#ADD A NEW COLUMN CALLED AGE :
+now = datetime.datetime.now()
+current_year = now.year 
+data['Age']= current_year - pd.DatetimeIndex(data['Date_of_Birth']).year
+
+
+Avg_age = data.groupby(data['Specialization'])['Age'].mean()
+
+
+print(Avg_age)
+
+
+# In[45]:
+
+
+#Retrieve the total number of students in each field of study.
+Total_students = data.groupby('Field_of_Study')['Student_ID'].nunique()
+print(Total_students)
+
+
+# In[96]:
+
+
+#Find the number of students who are expected to graduate in each year.
+
+
+grad_students = data.groupby(data['Expected_Year_of_Graduation'])['Student_ID'].nunique()
+print(grad_students)
+        
+#graduating_students()
+
+gs = pd.DataFrame({'Year_of _Gradution' : grad_students.index,  'Students' : grad_students.values})
+#graduation = gs['Year_of _Gradution']
+
+gs.plot(kind = 'line', x = 'Year_of _Gradution' , y = 'Students', color = 'blue', marker = '*')
+plt.xlabel('Year of Graduation')
+plt.ylabel('Number of students')
+plt.ylim(28000, 29000)
+plt.show()
+
+
+# In[56]:
+
+
+#Determine the total fees collected for each year of admission.
+
+Total_fees = data.groupby('Year_of_Admission')['Fees'].sum()
+#print(Total_fees)
+
+
+Tf = pd.DataFrame({'Year_of_Admission': Total_fees.index, 'Fees': Total_fees.values})
+
+fee = Tf['Fees'].values.tolist()
+#print(fee)
+yearr = Tf['Year_of_Admission'].values.tolist()
+#print(yearr)
+
+#Visualization
+colors = ['Blue' , 'Green', 'Yellow', 'Orange' , 'Indigo', 'Violet', 'Red']
+Tf.plot(kind= 'bar', x = 'Year_of_Admission', y ='Fees', color = colors)
+plt.xlabel ('Year of Admission')
+plt.ylabel('Total Fees in millions')
+plt.show()
+
+
+
+# In[59]:
+
+
+#Calculate the average discount on fees for each field of study.
+
+avg_discount = data.groupby('Field_of_Study')['Discount_on_Fees'].mean()
+Rounded_discount = round(avg_discount , 2)
+print(Rounded_discount)
+
+
+# In[70]:
+
+
+#Identify the student(s) with the highest discount on fees.
+
+student_discount = data.groupby('Student_ID')['Discount_on_Fees'].sum()
+#print(student_discount)
+
+sd = pd.DataFrame(student_discount)
+
+top_five =  sd.nlargest(5, 'Discount_on_Fees')
+print(top_five)
+
+
+# In[83]:
+
+
+#Find the field of study with the highest total fees collected.
+
+highest_fees = data.groupby('Field_of_Study')['Fees'].sum()
+hs = pd.DataFrame({'Field': highest_fees.index ,  'Total_fees' : highest_fees.values})
+print(hs)
+
+#VISUALIZATION
+
+hs.plot(kind = 'bar' , x = 'Field', y =  'Total_fees', color = ('blue', 'green', 'orange', 'yellow', 'Violet'), )
+plt.xlabel('Field')
+plt.ylabel('Total Fees in Billions')
+plt.title('highest total fees per field')
+plt.show()
+
+
+# In[114]:
+
+
+
+#Identify the students who have the same field of study, specialization, and expected year of graduation.
+grouped_students = data.groupby(['Field_of_Study', 'Specialization', 'Expected_Year_of_Graduation'])['Student_ID'].nunique()
+
+# Filter the groups where the count of unique student IDs is greater than 1
+same_students = grouped_students[grouped_students > 0]
+same_students = pd.DataFrame(same_students)
+same_students = same_students.sort_values(by = ['Expected_Year_of_Graduation'])
+
+pd.set_option('display.max_rows', None)
+same_students.head(100)
+
+
+# In[ ]:
+
+
+
+
